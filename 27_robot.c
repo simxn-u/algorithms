b4c4c18d8de5f57ef24cbad4c38dfd4df7948644
simxn-u <simxn@gmx.at>
2025-05-21 08:42:20 +0200

    Problem specification changed

--- 27_robot.c
+++ 27_robot.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +7,11 @@
 
 typedef struct {
   char dir;
-  char symbol;
+  int8_t dirX;
+  int8_t dirY;
 } Commands;
 
-Commands commands[] = {{'u', '^'}, {'d', 'v'}, {'l', '<'}, {'r', '>'}};
+Commands commands[] = {{'^', -1, 0}, {'<', 0, -1}, {'>', 0, 1}, {'v', 1, 0}};
 
 int main() {
   char input[MAXLEN];
@@ -18,14 +20,27 @@ int main() {
   uint32_t len = strlen(input) - 1;
   input[len] = '\0';
 
-  char *token = strtok(input, " ");
-  while (token != NULL) {
-    for (int i = 0; i < 4; i++) {
-      if (token[0] == commands[i].dir) {
-        printf("%c", commands[i].symbol);
+  uint8_t grid[MAXLEN * 2][MAXLEN * 2] = {0};
+  uint8_t x = (uint8_t)MAXLEN, y = (uint8_t)MAXLEN;
+  grid[x][y] = 1;
+
+  for (uint32_t i = 0; i < len; i++) {
+    char c = input[i];
+    for (int j = 0; j < 4; j++) {
+      if (c == commands[j].dir) {
+        grid[x += commands[j].dirX][y += commands[j].dirY]++;
         break;
       }
     }
-    token = strtok(NULL, " ");
   }
+
+  uint8_t maxVisits = 0;
+  for (int i = 0; i < MAXLEN * 2; i++) {
+    for (int j = 0; j < MAXLEN * 2; j++) {
+      if (grid[i][j] > maxVisits) {
+        maxVisits = grid[i][j];
+      }
+    }
+  }
+  printf("Max visits: %d\n", maxVisits);
 }
